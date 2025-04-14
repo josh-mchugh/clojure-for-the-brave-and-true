f4d5244fb6fccabe43799893dbcf7090553969fc
Josh McHugh <josh.mchugh@mailbox.org>
2025-04-14 00:06:13 -0400

    example code

--- clojure-noob-ch4/src/clojure_noob_ch4/core.clj
+++ clojure-noob-ch4/src/clojure_noob_ch4/core.clj
@@ -4,3 +4,15 @@
 (defn titleize
   [topic]
   (str topic " for the Brave and True"))
+
+(def human-consumption [8.1 7.3 6.6 5.0])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+(defn unify-diet-data
+  [human critter]
+  {:human human :critter critter})
+
+(def identities
+  [{:alias "Batman" :real "Bruce Wayne"}
+   {:alias "Spider-Man" :real "Peter Parker"}
+   {:alias "Santa" :real "Your mom"}
+   {:alias "Easter Bunny" :real "Your dad"}])
