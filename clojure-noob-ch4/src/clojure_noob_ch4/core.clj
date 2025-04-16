55211119b34ee7debd7d39479d07bc5e3906ef2e
Josh McHugh <josh.mchugh@mailbox.org>
2025-04-16 00:00:15 -0400

    More book examples

--- clojure-noob-ch4/src/clojure_noob_ch4/core.clj
+++ clojure-noob-ch4/src/clojure_noob_ch4/core.clj
@@ -26,3 +26,25 @@
    {:month 3 :day 2 :human 4.0 :critter 3.8}
    {:month 4 :day 1 :human 3.7 :critter 3.9}
    {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(def vampire-database
+  {0 {:makes-blood-puns? false :has-pulse? true :name "McFishwich"}
+   1 {:makes-blood-puns? false :has-pulse? true :name "McMackson"}
+   2 {:makes-blood-puns? true, :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true, :has-pulse? true :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
