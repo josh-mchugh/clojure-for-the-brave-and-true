556d972c6f229f1be30e793c76179730fbf3e9c2
Josh McHugh <josh.mchugh@mailbox.org>
2025-04-21 23:32:49 -0400

    coping some examples from chapter 6

---
+++ clojure-noob-ch6/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-noob-ch6
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure-noob-ch6/src/clojure_noob_ch6/core.clj
@@ -0,0 +1,26 @@
+(ns clojure-noob-ch6.core
+  (:gen-class))
+
+;; Create a vector of titles
+(def great-books ["East of Eden" "The Glass Bead Game"])
+
+;; Set the value again with 'name collision'
+(def great-books ["The Power of Bees" "Journey to Upstairs"])
+
+;; Create a new namespace
+(create-ns 'cheese.taxonomy)
+
+;; this command creates a namespace if it does exists and switches you to it
+(in-ns 'cheese.taxonomy)
+
+;; creates fine vector of cheddars
+(def cheddars ["mild" "medium" "strong" "sharp" "extra sharp"])
+
+;; create a list vector bries
+(def bries ["Wisconsin" "Somerset" "Brie de Meaux" "Brie de Melum"])
+
+;; example of private function in Clojure
+;; notice the dash after 'defn'
+(defn- private-function
+  "Just an example function that does nothing"
+  [])
